65f7e9dd47fbd8a79301451582bf306fced6657d
Gibson <gibsonxhan@gmail.com>
2019-10-19 15:46:19 -0700

    exercise 3.15 phonebook database removing phonecontact

--- part3/index.js
+++ part3/index.js
@@ -62,9 +62,15 @@ app.get('/api/persons/:id', (request, response) => {
 })
 
 app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(person => person.id !== id)
-    response.status(204).end()
+    Person.findByIdAndRemove(request.params.id)
+        .then(result => {
+            response.status(204).end()
+        })
+        .catch(error => {
+            response.status(404).send({
+                error: 'failed to delete'
+            })
+        })
 })
 
 const generateID = () => {
--- part3/models/person.js
+++ part3/models/person.js
@@ -2,9 +2,11 @@ const mongoose = require('mongoose')
 
 const url = process.env.MONGODB_URI
 
+mongoose.set('useFindAndModify', false)
+
 console.log('Connecting to..', url)
 
-mongoose.connect(url, {  useNewUrlParser: true})
+mongoose.connect(url, {  useNewUrlParser: true, useUnifiedTopology: true })
     .then(result => {
         console.log('Connected to MongoDB')
     })
