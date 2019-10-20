2452fcc0e284b0cd118c569b81f0da16640172e4
Gibson <gibsonxhan@gmail.com>
2019-10-20 16:01:28 -0700

    exercise 4.2 refactoring folder structure

---
+++ part4/utils/config.js
@@ -0,0 +1,4 @@
+const Port = 3003
+const MongoUrl = 'mongodb+srv://fullstackNoob:fullstacknoob123@cluster0-guhsy.mongodb.net/bloglist?retryWrites=true&w=majority'
+
+module.exports = { MongoUrl, Port }
