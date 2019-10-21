17c6b6da7c2913067fc56f75772d289731f3b6d1
Gibson <gibsonxhan@gmail.com>
2019-10-20 17:57:51 -0700

    exercise 4.6 - 4.7

--- part4/_test/listHelper.test.js
+++ part4/_test/listHelper.test.js
@@ -1,6 +1,7 @@
 const listHelper = require('../utils/list_helper')
+const data = require('./testData')
 
- describe('list helper test', () => {
+ describe('list helper Dummy Test', () => {
         test('dummy test returns one', () => {
             const blogs = []
             
@@ -8,3 +9,28 @@ const listHelper = require('../utils/list_helper')
             expect(result).toBe(1)
         })
     })
+
+describe('total likes', () => {
+    test('of empty list is zero', () => {
+        const zeroBlogs = []
+        const result = listHelper.totalLikes(zeroBlogs)
+        expect(result).toBe(0)
+    })
+
+    test('when list has only one blog equals the likes of that', () => {
+        const result = listHelper.totalLikes(data.listWithOneBlog)
+        expect(result).toBe(5)
+    })
+
+    test('when list has only one blog equals the likes of that', () => {
+        const result = listHelper.totalLikes(data.blogs)
+        expect(result).toBe(36)
+    })  
+})
+
+describe('Favorite Blog', () => {
+    test('Favorite blog check is titled: Canonical String Reduction by Mr. E . Dijkstrsa', () => {
+        const result = listHelper.favoriteBlog(data.blogs)
+        expect(result).toEqual(data.favorite)
+    })
+})
--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -4,6 +4,40 @@ const dummy = (blogs) => {
         : blogs.length 
 }
 
+const totalLikes = (blogs) => {
+    if(blogs.length === 0) return 0
+    else if(blogs.length === 1) return blogs[0].likes
+    else {
+        return blogs.reduce((total, curr) => {
+            return total + curr.likes
+        }, 0)
+    }
+}
+
+const favoriteBlog = (blogs) => {
+    //iterate through list
+    //find the biggest algro?
+    //return object with most like
+
+    return blogs.reduce((_fav, curr) => {
+        if (curr.likes > _fav.likes) {
+            _fav = {
+                title: curr.title,
+                author: curr.author,
+                likes: curr.likes
+            }
+        }
+        
+        return _fav
+    }, {
+        title: "",
+        author: "",
+        likes: 0
+    } )
+}
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes,
+    favoriteBlog
 }
